7f78ce531d77afa12c26a7d7516bed66c4047422
reklama-expert@yandex.ru <reklama-expert@yandex.ru>
2022-08-15 00:20:19 +0300

    PR13_test_2.2

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,38 +1,18 @@
 const { User } = require('../models/user');
 
-const ERROR_CODE = 400;
-
 exports.getUsers = async (req, res) => {
   const users = await User.find({});
   res.send(users);
 };
 
 exports.getUserbyId = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    res.send(user);
-  } catch (err) {
-    if (err.name === 'CastError') {
-      return res.status(ERROR_CODE).send({
-        message: 'Запрашиваемый пользователь не найден',
-      });
-    }
-    console.error(`ОШИБКА ${err.name}`);
-  }
+  const user = await User.findById(req.params.id);
+  res.send(user);
 };
 
 exports.createUser = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.send(user);
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      return res.status(ERROR_CODE).send({
-        message: 'Получены неверные данные',
-      });
-    }
-    console.error(`ОШИБКА ${err.name}`);
-  }
+  const user = await User.create(req.body);
+  res.send(user);
 };
 
 exports.updateUser = async (req, res) => {
